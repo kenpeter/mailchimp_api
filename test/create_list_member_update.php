d33861ae8df861cacf99c09fbb6a749356986fae
kenpeter <figo2476@hotmail.com>
2017-04-19 00:10:41 +1000

    update

--- test/create_list_member_update.php
+++ test/create_list_member_update.php
@@ -3,44 +3,55 @@
 // auto load
 require '../vendor/autoload.php';
 
-// my lib
-require '../lib/MailchimpClient.php';
+use GuzzleHttp\Psr7\Request;
 
-// use it
-use \Classy\MailchimpClient;
+// opt
+$option = array(
+  'base_uri' => "https://us12.api.mailchimp.com/3.0/",
+  'auth' => ['apikey', '292bae37c631ac3ba03ed0640b44e6c3'],
+);
 
-// my config
-$config = include('../config.php');
+// client
+$client = new \GuzzleHttp\Client($option);
 
-// init
-$client = new MailchimpClient($config->apikey);
+// data for a new list
+$data = array(
+  "name" => "test_mailchimp",
+  "contact" => array(
+    "company" => "MailChimp",
+    "address1" => "675 Ponce De Leon Ave NE",
+    "address2" => "Suite 5000",
+    "city" => "Atlanta",
+    "state" => "GA",
+    "zip" => "30308",
+    "country" => "US",
+    "phone" => "12345678",
+  ),
+  "permission_reminder" => "You're receiving this email because you signed up for updates.",
+  "use_archive_bar" => true,
+  "campaign_defaults" => array(
+    "from_name" => "test",
+    "from_email" => "test@test.com",
+    "subject" => "test_subject",
+    "language" => "en",
+  ),
+	"notify_on_subscribe" => "",
+	"notify_on_unsubscribe" => "",
+	"email_type_option" => true,
+	"visibility" => "pub",
+);
 
-// lists request
-$req_list = new \GuzzleHttp\Psr7\Request('GET', 'lists');
+$headers = array(
+  'User-Agent' => 'testing/1.0',
+  'Accept'     => 'application/json'
+);
+$body = ['json' => $data];
+$req_create_list = new Request('POST', 'lists', $headers, $body);
 
 // promise
 $promise = $client
-  ->sendAsync($req_list)
+  ->sendAsync($req_create_list)
   ->then(function ($res) {
-    $obj = json_decode( $res->getBody() );
-    // always get the first list
-    $first_list_id = $obj->lists[0]->id;
-    echo $first_list_id;
-
+    echo $res->getBody();
   });
 $promise->wait();
-
-
-/*
-$data = array(
-  'email_address' => 'member@member.com',
-  "status" => "subscribed"
-);
-
-// It seems api is a bit slow, after post the request....
-// need to wait for the result for the web interface.
-$list_id = '1309575198';
-$uri = 'lists/'. $list_id. '/members';
-$return = $client->request('POST', $uri, ['json' => $data]);
-var_dump($return);
-*/
