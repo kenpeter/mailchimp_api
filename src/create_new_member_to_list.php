405ac40d04d4593323bddf8c2d8fad41ac90ff3b
kenpeter <figo2476@hotmail.com>
2017-04-18 16:23:04 +1000

    update

---
+++ src/create_new_member_to_list.php
@@ -0,0 +1,29 @@
+<?php
+
+// auto load
+require '../vendor/autoload.php';
+
+// my lib
+require '../lib/MailchimpClient.php';
+
+// use it
+use \Classy\MailchimpClient;
+
+// my config
+$config = include('../config.php');
+
+// init
+$client = new MailchimpClient($config->apikey);
+
+
+$data = array(
+  'email_address' => 'member@member.com',
+  "status" => "subscribed"
+);
+
+// It seems api is a bit slow, after post the request....
+// need to wait for the result for the web interface.
+$list_id = '1309575198';
+$uri = 'lists/'. $list_id. '/members';
+$return = $client->request('POST', $uri, ['json' => $data]);
+var_dump($return);
--- test/curl.php
+++ test/curl.php
@@ -1,7 +1,19 @@
+create a new list
+-----------------------------------
 curl --request POST \
 --url 'https://usX.api.mailchimp.com/3.0/lists' \
 --user 'anystring:apikey' \
 --header 'content-type: application/json' \
 --data '{"name":"Freddie'\''s Favorite Hats","contact":{"company":"MailChimp","address1":"675 Ponce De Leon Ave NE","address2":"Suite 5000","city":"Atlanta","state":"GA","zip":"30308","country":"US","phone":""},"permission_reminder":"You'\''re receiving this email because you signed up for updates about Freddie'\''s newest hats.","campaign_defaults":{"from_name":"Freddie","from_email":"freddie@freddiehats.com","subject":"","language":"en"},"email_type_option":true}' \
 --include
-           
+
+
+
+create a new member
+-----------------------------------
+curl --request POST \
+--url 'https://usX.api.mailchimp.com/3.0/lists/57afe96172/members' \
+--user 'anystring:apikey' \
+--header 'content-type: application/json' \
+--data '{"email_address":"urist.mcvankab+3@freddiesjokes.com", "status":"subscribed"}' \
+--include
